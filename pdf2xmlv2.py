0eaa7530b61a02e77ab89bcd13bd89804cdee425
Adam <adamnasir24@gmail.com>
2023-04-04 14:46:49 +0800

    Added xml viewer fx

--- pdf2xmlv2.py
+++ pdf2xmlv2.py
@@ -5,7 +5,7 @@ from PIL import Image
 import re
 import os
 import tkinter as tk
-from tkinter import filedialog, messagebox, scrolledtext
+from tkinter import filedialog
 
 def pdf_to_xml(file_path):
     pdf_file = open(file_path, 'rb')
@@ -48,62 +48,30 @@ def file_to_xml(file_path):
         xml_text += text
         xml_text += '</page>\n'
         xml_text += '</document>'
-    elif file_path.endswith('.xml'):
-        with open(file_path, 'r') as xml_file:
-            xml_text = xml_file.read()
     else:
         return None
 
     # Save XML text to file
-    if not file_path.endswith('.xml'):
-        xml_file_path = os.path.splitext(file_path)[0] + '.xml'
-        with open(xml_file_path, 'w') as xml_file:
-            xml_file.write(xml_text)
-    else:
-        xml_file_path = file_path
+    xml_file_path = os.path.splitext(file_path)[0] + '.xml'
+    with open(xml_file_path, 'w') as xml_file:
+        xml_file.write(xml_text)
 
-    return xml_text, xml_file_path
+    return xml_text
 
 def browse_file():
     file_path = filedialog.askopenfilename()
     if file_path:
-        xml_text, xml_file_path = file_to_xml(file_path)
+        xml_text = file_to_xml(file_path)
         if xml_text:
             success_label.config(text='Conversion successful!')
-            show_xml_editor(xml_text, xml_file_path)
-
-def open_xml():
-    file_path = filedialog.askopenfilename(filetypes=[('XML Files', '*.xml')])
-    if file_path:
-        xml_text, xml_file_path = file_to_xml(file_path)
-        if xml_text:
-            show_xml_editor(xml_text, xml_file_path)
-
-def show_xml_editor(xml_text, file_path):
-    def save_xml():
-        modified_xml_text = xml_editor.get('1.0', 'end')
-        with open(file_path, 'w') as xml_file:
-            xml_file.write(modified_xml_text)
-        messagebox.showinfo('Save', 'XML file saved successfully')
-
-    xml_editor_window = tk.Toplevel(root)
-    xml_editor_window.title('XML Editor')
-
-    xml_editor = scrolledtext.ScrolledText(xml_editor_window, width=80, height=30)
-    xml_editor.pack(padx=10, pady=10)
-    xml_editor.insert('1.0', xml_text)
-
-    save_button = tk.Button(xml_editor_window, text='Save', command=save_xml)
-    save_button.pack(pady=10)
+        else:
+            success_label.config(text='Invalid file type')
 
 root = tk.Tk()
 root.title('PDF/DOCX to XML Converter')
 
 upload_button = tk.Button(root, text='Upload File', command=browse_file)
 upload_button.pack(pady=20)
-
-open_button = tk.Button(root, text='Open XML', command=open_xml)
-open_button.pack(pady=10)
 
 success_label = tk.Label(root, text='')
 success_label.pack()
